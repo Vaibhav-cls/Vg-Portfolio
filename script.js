e32cf56b6c1ddbb695186dd693e5092a0a455b2a
Vaibhav-cls <63966372+Vaibhav-cls@users.noreply.github.com>
2023-05-06 12:34:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var x = document.getElementById("fname");
+var y = document.getElementById("lname");
+function color_change() {
+    x.style.color = "black";
+    y.style.color = "#f44336";
+}
+function original() {
+    x.style.color = "#f44336";
+    y.style.color = "black";
+}
