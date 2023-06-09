251d260319b5875d25d1c53265d84eaeae56fe18
Vaibhav-cls <63966372+Vaibhav-cls@users.noreply.github.com>
2023-06-09 12:23:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var noDragElements = document.querySelectorAll('.no-drag');
+
+    for (var i = 0; i < noDragElements.length; i++) {
+      noDragElements[i].addEventListener('dragstart', function(e) {
+        e.preventDefault();
+      });
+    }
+  });
+
 var x = document.getElementById("fname");
 var y = document.getElementById("lname");
+let toggler="black";
 function color_change() {
-    x.style.color = "black";
+    x.style.color = toggler;
     y.style.color = "#f44336";
 }
 function original() {
     x.style.color = "#f44336";
-    y.style.color = "black";
+    y.style.color = toggler;
 }
+
+var button = document.getElementById('mode');
+var sheets = document.getElementById('styleSheet');
+button.addEventListener("click", function () {
+    if (sheets.getAttribute("href") === "style.css") {
+        button.innerHTML = "dark_mode";
+        sheets.setAttribute("href","dark.css");
+        toggler="white";
+        color_change();
+        original();
+    }
+    else{
+        button.innerHTML="light_mode";
+        sheets.setAttribute("href","style.css");
+        toggler="black";
+        color_change();
+        original();
+    }
+})
+let Reload = document.getElementById("reload");
+function rel(){
+    location.reload();
+}
+
